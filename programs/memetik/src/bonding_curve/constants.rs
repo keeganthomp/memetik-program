40a39ff0834defc149803731634f21782887be65
Keegan Thompson <whereiskeegan@gmail.com>
2024-07-02 13:31:49 -0700

    unused

--- programs/memetik/src/bonding_curve/constants.rs
+++ programs/memetik/src/bonding_curve/constants.rs
@@ -7,6 +7,4 @@ pub const SECONDS_IN_A_DAY: i64 = SECONDS_IN_A_HOUR * 24;
 pub const REQUIRED_ESCROW_AMOUNT: u64 = 0 * LAMPORTS_PER_SOL; // in lamports
 pub const DAYS_TO_MATURITY: i64 = 2; // number of days the pool has to reach milestone
 
-pub const TOKEN_SCALE: f64 = 1e18;
-
 pub const REQUIRED_MARKET_CAP_TO_MATURE: f64 = 10_000.0; // is USD (i.e 1000.0 is $1000.0)
--- programs/memetik/src/bonding_curve/utils.rs
+++ programs/memetik/src/bonding_curve/utils.rs
@@ -4,6 +4,8 @@ use anchor_lang::solana_program::sysvar::clock::Clock;
 use anchor_lang::solana_program::sysvar::Sysvar;
 use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
 
+pub const TOKEN_SCALE: f64 = 1e18;
+
 pub fn check_valid_ticker(s: &str) -> bool {
     if s.is_empty() {
         return false;
@@ -13,10 +15,6 @@ pub fn check_valid_ticker(s: &str) -> bool {
 
 pub fn to_whole_units(lamport_amount: u64) -> f64 {
     lamport_amount as f64 / LAMPORTS_PER_SOL as f64
-}
-
-pub fn to_atomic_units(amount: f64) -> u64 {
-    (amount * TOKEN_SCALE).round() as u64
 }
 
 pub fn calculate_maturity_time() -> i64 {
